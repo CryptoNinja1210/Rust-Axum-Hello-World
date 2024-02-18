e2e81da5a4301fe89c6cca5f8979f78aafe3b12a
levan <rockdevmufasa@gmail.com>
2024-02-19 07:44:49 +0800

    first

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+use tower_http::cors::{Any, CorsLayer};
+
+#[tokio::main]
+async fn main() {
+    let cors = CorsLayer::new().allow_origin(Any);
+
+    let app = Router::new()
+        .route("/", get(root))
+        .layer(cors);
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
